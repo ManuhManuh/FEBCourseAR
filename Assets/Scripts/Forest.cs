c114d78712cc682910db1ab90fdc3109bdd07ab5
ManuhManuh <72955291+ManuhManuh@users.noreply.github.com>
2021-09-25 03:41:34 -0600

    Initial commit

---
+++ Assets/Scripts/Forest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DigitalRuby.RainMaker;
+
+public class Forest : MonoBehaviour
+{
+    public bool raining;
+    public TMP_Text forecastText;
+    public RainScript rainEffect;
+    public List<Animator> treeAnimators = new List<Animator>();
+
+    private void Start()
+    {
+        raining = false;
+    }
+    public void ToggleRain()
+    {
+        // Toggle the rain
+        raining = !raining;
+
+        // Update the forecast text
+        forecastText.text = raining ? "Forecast: \nRain" : "Forecast: \nCalm";
+
+        // Enable or disable the rain effect
+        rainEffect.RainIntensity = raining ? 1f : 0f;
+
+        // For each tree in the forest
+        foreach(Animator treeAnimator in treeAnimators)
+        {
+            // Enable or disable the animator depending on the weather
+            treeAnimator.enabled = raining;
+        }
+
+    }
+}
---
+++ Assets/Scripts/GrassPlanter.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GrassPlanter : MonoBehaviour
+{
+    public Transform placeholderGrass;
+    public Transform placeholderFlower;
+    public float plantingGrassDuration;
+    public Button plantGrassButton;
+    public float timeBetweenGrassPlants;
+    public float plantingRadius;
+    public int oddsOfFlower;
+
+    private bool planting;
+    private float timeLeftPlantingGrass;
+    private float timeLeftUntilNextGrassPlant;
+
+    public void StartPlantingGrass()
+    {
+        // Mark as planting grass
+        planting = true;
+
+        // Reset the time left planting grass
+        timeLeftPlantingGrass = plantingGrassDuration;
+
+        // Disable plant grass button
+        plantGrassButton.interactable = false;
+    }
+
+    private void Update()
+    {
+        // If we're planting grass
+        if (planting)
+        {
+            // Count down the time left planting grass
+            timeLeftPlantingGrass -= Time.deltaTime;
+
+            // If we should stop planting grass (timer has run out)
+            if (timeLeftPlantingGrass <= 0f)
+            {
+                // Stop planting grass
+                StopPlantingGrass();
+     
+            }
+
+            // Countdown the time until we should plant the next grass
+            timeLeftUntilNextGrassPlant -= Time.deltaTime;
+
+            // If it is time to plant a new grass
+            if(timeLeftUntilNextGrassPlant <= 0f)
+            {
+                // Determine whether we are going to plant grass or a flower
+                if (Random.Range(0, oddsOfFlower) == 0)
+                {
+                    // Plant a flower
+                    PlantFlower();
+                }
+                // Plant a grass
+
+                PlantGrass();
+
+                // Reset grass planting timer 
+                timeLeftUntilNextGrassPlant = timeBetweenGrassPlants;
+            }
+
+        }
+
+    }
+
+    private void StopPlantingGrass()
+    {
+        // Unmark as planting
+        planting = false;
+
+        // Enable the plant grass button
+        plantGrassButton.interactable = true;
+    }
+    public void PlantGrass()
+    {
+        // Calculate a random offset position for the grass
+        Vector3 randomOffset = Random.insideUnitSphere * plantingRadius;
+        randomOffset.y = 0f;
+
+        // Make a clone of the placeholder grass
+        Transform newGrass = Instantiate(placeholderGrass, placeholderGrass.position + randomOffset, placeholderGrass.rotation);
+        newGrass.SetParent(null, true);
+    }
+
+    public void PlantFlower()
+    {
+        // Calculate a random offset position for the grass
+        Vector3 randomOffset = Random.insideUnitSphere * plantingRadius;
+        randomOffset.y = 0f;
+
+        // Make a clone of the placeholder grass
+        Transform newFlower = Instantiate(placeholderFlower, placeholderFlower.position + randomOffset, placeholderFlower.rotation);
+        newFlower.SetParent(null, true);
+    }
+}
